2229dbce1655b60bebddb716900e2e75ddbfa289
Luke Parker <lukeparker@Lukes-MacBook-Pro.local>
2020-11-27 22:36:17 -0800

    users broadcast instead of emit to everyone

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var app = require('express')();
-var http = require('http').createServer(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        socket.broadcast.emit('chat message', msg);
+    });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
